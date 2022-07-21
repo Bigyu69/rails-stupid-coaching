a16cf7905f5a86c6a98bebe5bac93be30fb4c755
yulin zhou <zhouyul.uk@gmail.com>
2022-07-21 19:01:10 +0100

    answers working

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,13 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    questions = params[:askquestion]
-    raise
+    @question = params[:askquestion]
+    if @question[-1] == "?"
+      @answer = "Silly question, get dressed and go to work!"
+    elsif @question == "I am going to work"
+      @answer = "Great!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
