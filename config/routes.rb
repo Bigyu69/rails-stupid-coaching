e30db7a6fc233a420e10006a2ae68c8fe3eb2ad9
yulin zhou <zhouyul.uk@gmail.com>
2022-07-21 01:06:47 +0100

    latest

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # Create a new Rails app
+  # Launch a web server to open your app locally
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/ask", to: "questions#ask"
+  get "/answer", to: "questions#answer"
+
 end
